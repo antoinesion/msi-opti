edbca7d987eabc15b1b9024cd60825553cedf07c
Antoine Sion <antoine.sion@etu.minesparis.psl.eu>
2022-09-23 12:15:38 +0200

    v2

--- src/includes/transfo.c
+++ src/includes/transfo.c
@@ -3,8 +3,8 @@ void transfo(int w, int h, unsigned char *src, unsigned char *dest, unsigned cha
   	int i,j;
 	unsigned char curved;
 
-  	for (i = 0; i < w; i++) {
-		for (j = 0; j < h; j++) {
+  	for (j = 0; j < h; j++) {
+   		for (i = 0; i < w; i++) {
 			curved = lut[src[j * w + i]];
 			dest[j * w + i] = (((int) curved + val) > 255) ? 255 : curved + val;
 		}
